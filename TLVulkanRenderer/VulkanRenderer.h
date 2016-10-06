1e7a19f7a849e47c8dfb60964dd8a4482056e360
Trung Le <trungtuanle90@gmail.com>
2016-10-06 00:29:04 -0400

    Query swapchain support and setting swapchaing surface format, present mode, and extent

--- TLVulkanRenderer/VulkanRenderer.h
+++ TLVulkanRenderer/VulkanRenderer.h
@@ -46,6 +46,16 @@ struct SwapchainSupport {
 	}
 };
 
+struct SwapchainDesiredSetting {
+
+	// Present mode preference
+
+	// Surface format preference
+	//std::vector<VkSurfaceFormatKHR> desiredSurfaceFormats;
+
+	// Surface extent
+};
+
 /**
  * \brief 
  */
@@ -100,6 +110,9 @@ private:
 	SelectPhysicalDevice();
 	
 	VkResult
+	CreateSwapchain();
+
+	VkResult
 	SetupLogicalDevice();
 
 	void 
@@ -145,3 +158,72 @@ QuerySwapchainSupport(
 	, const VkSurfaceKHR& surface
 	);
 
+/**
+ * \brief The surface format specifies color channel and types, and the color space
+ * \param availableFormats 
+ * \return the VkSurfaceFormatKHR that we desire.
+ * \ref https://www.khronos.org/registry/vulkan/specs/1.0-wsi_extensions/xhtml/vkspec.html#VkSurfaceFormatKHR
+
+ */
+VkSurfaceFormatKHR
+SelectDesiredSwapchainSurfaceFormat(
+	const std::vector<VkSurfaceFormatKHR> availableFormats
+	);
+
+/**
+ * \brief This is the most important setting for the swap chain. This is how we select
+ *		  the condition when the image gets presented to the screen. There are four modes:
+ *		  VK_PRESENT_MODE_IMMEDIATE_KHR: 
+ *				Image gets presented right away without vertical blanking. Tearing could occur. 
+ *				No internal queuing requests required.
+ *				
+ *		  VK_PRESENT_MODE_MAILBOX_KHR:
+ *				Waits for the next vertical blank. No tearing. 
+ *				A single-entry internal queue is used to hold the pending request.
+ *				It doesn't block the application when the queue is full, instead
+ *				new request replaces the old one inside the queue.
+ *				One request per vertical blank interval.
+ *				This can be used to implement tripple buffering with less latency and no tearing
+ *				than the standard vertical sync with double buffering.
+ *				
+ *		  VK_PRESENT_MODE_FIFO_KHR (guaranteed to be supported):
+ *				Waits for the next vertical blank. No tearing. 
+ *				An internal queue is used to hold the pending requests. 
+ *				New requests are queued at the end. If the queue is full,
+ *				this will block the application.
+ *				One request per vertical blank interval.
+ *				Most similar to vertical sync in modern games.
+ *				
+ *		  
+ *		  VK_PRESENT_MODE_FIFO_RELAXED_KHR:
+ *				This generally waits for a vertical blank. But if one has occurs
+ *				and the image arrives late, then it will present the next image right away.
+ *				This mode also uses an internal queue to hold the pending requests.
+ *				Use this when the images stutter.
+ *				Tearing might be visible.
+ *				
+ * \param availablePresentModes 
+ * \return the VkPresentModeKHR that we desire
+ * \ref https://www.khronos.org/registry/vulkan/specs/1.0-wsi_extensions/xhtml/vkspec.html#VkPresentModeKHR
+ */
+VkPresentModeKHR
+SelectDesiredSwapchainPresentMode(
+	const std::vector<VkPresentModeKHR> availablePresentModes
+	);
+
+/**
+ * \brief 
+ * \param surfaceCapabilities 
+ * \param useCurrentExtent: this should be true for most cases
+ * \param desiredWidth: we can differ from the current extent if chosen so 
+ * \param desiredHeight: we can differ from the current extent if chosen so 
+ * \return 
+ * \ref https://www.khronos.org/registry/vulkan/specs/1.0-wsi_extensions/xhtml/vkspec.html#VkSurfaceCapabilitiesKHR
+ */
+VkExtent2D
+SelectDesiredSwapchainExtent(
+	const VkSurfaceCapabilitiesKHR surfaceCapabilities
+	, bool useCurrentExtent = true
+	, unsigned int desiredWidth = 0 /* unused if useCurrentExtent is true */
+	, unsigned int desiredHeight = 0 /* unused if useCurrentExtent is true */
+);
