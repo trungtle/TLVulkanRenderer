43f002184414c003195f94382bf837a4cc18b42e
Trung Le <trungtuanle90@gmail.com>
2017-01-24 15:39:01 -0500

    [raytracing] - Cleaned up code. Displayed a texture image on screen

--- TLVulkanRenderer/shaders/raytracing/generateSPIRV.bat
+++ TLVulkanRenderer/shaders/raytracing/generateSPIRV.bat
@@ -1,3 +1,5 @@
+glslangvalidator -V -t quad.vert -o quad.vert.spv
+glslangvalidator -V -t quad.frag -o quad.frag.spv
 glslangvalidator -V -t raytrace.comp -o raytrace.comp.spv
 glslangvalidator -V -t raytrace.frag -o raytrace.frag.spv
 glslangvalidator -V -t raytrace.vert -o raytrace.vert.spv
